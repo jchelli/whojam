6e994004e2a6da707818b39b5aa2de0114030873
Jordan Chelli <jordan.chelli@gmail.com>
2013-07-18 18:33:16 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whojam::Application.config.secret_token = '3f6c32ea52a2b72b1a228b8d8e15dc3b6d8183c8849b0af49f158440b52dfc9baabc77ed737bbbf7df527857fe4bb95b58cf979d5e052dd1aa4ba5b70be5663d'
